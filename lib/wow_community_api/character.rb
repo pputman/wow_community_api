49fe9ff92ebb0a5d57fd24274dd3a0aff8e56340
pputman <putman.patrick@gmail.com>
2011-09-01 12:45:34 -0500

    fixed bug where would cause errors running delete on nil object if no char found

--- lib/wow_community_api/character.rb
+++ lib/wow_community_api/character.rb
@@ -3,9 +3,11 @@ module WowCommunityApi
     def self.find_by_realm_and_name(realm, name, *field)
       fields = { :fields => field.join(",") } unless field.empty?
       results = get("/character/#{realm}/#{name}", fields)
-
-      results['class_id'] = results.delete('class') # handle reserved word "class"
-      results.to_ostruct
+      if results
+        results['class_id'] = results.delete('class') # handle reserved word "class"
+        results.to_ostruct
+      else nil
+      end 
     end
   end
 end
